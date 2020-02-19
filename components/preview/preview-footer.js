949187b9f0c78217e0009c82e044f22cbcf2d92c
Elbert Bae <elbert.bae@gmail.com>
2020-02-18 22:09:16 -0800

    Remove button and box shadow from preview cards

--- components/preview/preview-footer.js
+++ components/preview/preview-footer.js
@@ -9,9 +9,6 @@ const PreviewFooter = ({post}) => {
         <img src={post.authors[0].profile_image}/>
         <p><i>By {post.authors[0].name}</i></p>
       </div>
-      <div className={styles.previewButton}>
-        <FilledButton>Read post...</FilledButton>
-      </div>
     </>
   )
 }
